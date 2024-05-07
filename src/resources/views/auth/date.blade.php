7160eaee832bb3d9f68ae6183deb274819143e06
fukumamire <rinka_karyou@hotmail.com>
2024-05-07 23:52:22 +0900

    date blade.php日付一覧の修正　日付一覧に関してのルート修正

--- src/resources/views/auth/date.blade.php
+++ src/resources/views/auth/date.blade.php
@@ -1,48 +1,65 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('auth/date.css') }}">
+  <link rel="stylesheet" href="{{ asset('auth/date.css') }}">
     
 @endsection
 @section('link')
-    <div class="header__links">
-        <a href="/">ホーム</a>
-        <a href="/attendance">日付一覧</a>
-        <form action="{{ route('logout') }}" method="POST">
+  <div class="header__links">
+    <a href="/">ホーム</a>
+    <a href="/attendance">日付一覧</a>
+    <form action="{{ route('logout') }}" method="POST">
         @csrf  
-        <button type="submit" class="logout-button">ログアウト</button>
+      <button type="submit" class="logout-button">ログアウト</button>
 
 
         {{-- <a href="{{ route('dateList') }}">日付一覧</a> --}}
-        
-    </div>
+    </form>    
+  </div>
 @endsection
 
 @section('content')
-    <form class="header__wrap" action= method="post">
-        @csrf
-        <button class="date__change-button" name="prevDate"><</button>
-        <input type="hidden" name="displayDate" value=>
-        <p class="header__text"></p>
-        <button class="date__change-button" name="nextDate">></button>
-    </form>
+ <form class="header__wrap" action="/the-date" method="post">
+    @csrf
+    <button class="date__change-button" name="prevDate"><</button>
+    <input type="hidden" name="displayDate" value=>
+    <p class="header__text"></p>
+    <button class="date__change-button" name="nextDate">></button>
+ </form>
 
-    <div class="table__wrap">
-        <table class="attendance__table">
-            <tr class="table__row">
-                <th class="table__header">名前</th>
-                <th class="table__header">勤務開始</th>
-                <th class="table__header">勤務終了</th>
-                <th class="table__header">休憩時間</th>
-                <th class="table__header">勤務時間</th>
-            </tr>
-            <tr class="table__row">
-                <td class="table__item"></td>
-                <td class="table__item"></td>
-                <td class="table__item"></td>
-                <td class="table__item"></td>
-                <td class="table__item"></td>
-            </tr>
-        </table> 
-        
+ <div class="table__wrap">
+    <table class="attendance__table">
+      <tr class="table__row">
+        <th class="table__header">名前</th>
+        <th class="table__header">勤務開始</th>
+        <th class="table__header">勤務終了</th>
+        <th class="table__header">休憩時間</th>
+        <th class="table__header">勤務時間</th>
+      </tr>
+      @foreach ($attendances as $attendance)
+        <tr class="table__row">
+          <td class="table__item">{{ $attendance->user->name }}</td>
+          <td class="table__item">{{ $attendance->start_work }}</td>
+          <td class="table__item">{{ $attendance->end_work }}</td>
+          <td class="table__item">
+            @foreach ($attendance->workBreaks as $break)
+                {{ $break->start_break }} - {{ $break->end_break }}<br>
+            @endforeach
+          </td>
+          <td class="table__item">
+          @php
+						$workTime = $attendance->end_work ? \Carbon\Carbon::parse($attendance->end_work)->diffInMinutes($attendance->start_work) : 0;
+						$breakTime = $attendance->workBreaks->sum(function ($break) {
+    					return $break->end_break ? \Carbon\Carbon::parse($break->end_break)->diffInMinutes($break->start_break) : 0;
+    					});
+    					$totalWorkTime = $workTime - $breakTime;
+    					$hours = floor($totalWorkTime / 60);
+    					$minutes = $totalWorkTime % 60;
+						@endphp
+            {{ $hours }}時間{{ $minutes }}分
+          </td>
+        </tr>
+      @endforeach
+    </table> 
+ </form>      
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,40 +19,29 @@ use App\Http\Controllers\AttendanceController;
 // 会員登録ページ
 Route::get('/register', function () {
     return view('auth.register');
-})->middleware('guest')->name('resister');;
+})->middleware('guest')->name('register');
 
 // ログインページ
 Route::get('/login', function () {
     return view('auth.login');
 })->middleware('guest')->name('login');
 
-
-
 // ログアウトのルート
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth')
     ->name('logout');
 
-
 // 打刻ページの表示
-Route::get('/', function () {
-    return view('auth.stamp');
-})->middleware('auth');
+Route::get('/', [AttendanceController::class, 'showStampPage'])->middleware('auth')->name('stamp');
 
+// 日付一覧
+Route::get('/attendance', [AttendanceController::class, 'attendanceList'])->middleware('auth')->name('attendance');
 
+// 勤務時間関係
+Route::post('/start-work', [AttendanceController::class, 'startWork'])->middleware('auth')->name('start-work');
+Route::post('/end-work', [AttendanceController::class, 'endWork'])->middleware('auth')->name('end-work');
+Route::post('/start-break', [AttendanceController::class, 'startBreak'])->middleware('auth')->name('start-break');
+Route::post('/end-break', [AttendanceController::class, 'endBreak'])->middleware('auth')->name('end-break');
 
-//日付一覧
-Route::get('/attendance', function () {
-    return view('auth.date');
-})->middleware('auth');
-
-
-
-//勤務時間関係
-Route::get('/', [AttendanceController::class, 'showStampPage'])->middleware('auth');
-Route::post('/start-work', [AttendanceController::class, 'startWork'])->middleware('auth');
-Route::post('/end-work', [AttendanceController::class, 'endWork'])->middleware('auth');
-Route::post('/start-break', [AttendanceController::class, 'startBreak'])->middleware('auth');
-Route::post('/end-break', [AttendanceController::class, 'endBreak'])->middleware('auth');
-
-Route::post('/the-date', [AttendanceController::class, 'attendanceList'])->name('the-date');
+// 特定の日付の勤務記録を表示
+Route::post('/the-date', [AttendanceController::class, 'attendanceList'])->middleware('auth')->name('the-date');
